bcd3d1aa9ddfc9522401e79f43273f69df4c6f34
zero <osmk@qq.com>
2015-07-21 14:40:16 +0800

    first commit

---
+++ GGCache/GGCache.h
@@ -0,0 +1,13 @@
+//
+//  GGCache.h
+//  GGCache
+//
+//  Created by __无邪_ on 15/7/21.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GGCache : NSObject
+
+@end
---
+++ GGCache/GGCache.m
@@ -0,0 +1,13 @@
+//
+//  GGCache.m
+//  GGCache
+//
+//  Created by __无邪_ on 15/7/21.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGCache.h"
+
+@implementation GGCache
+
+@end
